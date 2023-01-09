50f066602764ab22db5294c487edfd64ee3e612e
Kevin Tindall <kevinkjt2000@gmail.com>
2023-01-08 18:22:49 -0600

    rust: refactor to reduce repetition

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -36,53 +36,34 @@ impl GildedRose {
                 item.quality = 80;
                 continue;
             }
-            if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-            {
-                if item.quality > 0 {
-                    if item.name != "Sulfuras, Hand of Ragnaros" {
-                        item.quality = item.quality - 1;
-                    }
+
+            item.sell_in = item.sell_in - 1;
+
+            if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                if item.sell_in < 0 {
+                    item.quality = 0;
+                    continue;
+                }
+                item.quality = item.quality + 1;
+                if item.sell_in < 10 {
+                    item.quality = item.quality + 1;
+                }
+                if item.sell_in < 5 {
+                    item.quality = item.quality + 1;
                 }
             } else {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1;
-
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sell_in < 11 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if item.sell_in < 6 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if item.name != "Sulfuras, Hand of Ragnaros" {
-                item.sell_in = item.sell_in - 1;
-            }
-
-            if item.sell_in < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
+                if item.name == "Aged Brie" {
+                    item.quality = item.quality + if item.sell_in < 0 {2} else {1};
                 } else {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = item.quality - if item.sell_in < 0 {2} else {1};
                 }
+            }
+
+            if item.quality > 50 {
+                item.quality = 50;
+            }
+            if item.quality < 0 {
+                item.quality = 0;
             }
         }
     }
