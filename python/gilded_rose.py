178e693f451b2061230de88d25bf77d199342865
Kevin Tindall <kevinkjt2000@gmail.com>
2023-01-10 18:39:19 -0600

    python: Wrote tests and then refactored

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,38 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
 
+def clamp(num, lower, upper):
+    return max(lower, min(num, upper))
+
+
+class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+        def backstage_passes(item):
+            if item.sell_in > 10:
+                return 1
+            elif item.sell_in > 5:
+                return 2
+            elif item.sell_in > 0:
+                return 3
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                return -item.quality
+
+        for item in self.items:
+            if "Sulfuras" in item.name:
+                continue
+
+            if "Backstage passes" in item.name:
+                change_in_quality = backstage_passes(item)
+            elif "Aged Brie" == item.name:
+                change_in_quality = 1 if item.sell_in > 0 else 2
+            else:
+                change_in_quality = -1 if item.sell_in > 0 else -2
+
+            item.sell_in -= 1
+            item.quality = clamp(item.quality + change_in_quality, 0, 50)
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,79 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import Item, GildedRose, clamp
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_clamp_works(self):
+        self.assertEqual(1, clamp(1, 0, 2))
+        self.assertEqual(0, clamp(-1, 0, 2))
+        self.assertEqual(2, clamp(5, 0, 2))
+
+    def test_backstage_passes_gain_then_drop_to_zero_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 0)]
+        gilded_rose = GildedRose(items)
+
+        for expected in [5, 15, 30]:
+            gilded_rose.update_quality()
+            gilded_rose.update_quality()
+            gilded_rose.update_quality()
+            gilded_rose.update_quality()
+            gilded_rose.update_quality()
+            self.assertEqual(expected, items[0].quality)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    def test_normal_items_lose_quality_slowly_then_quickly_then_stops(self):
+        items = [
+            Item("+5 Dexterity Vest", 2, 6),
+            Item("Elixir of the Mongoose", 5, 7),
+        ]
+        gilded_rose = GildedRose(items)
+        expectations = [
+            (5, 6),
+            (4, 5),
+            (2, 4),
+            (0, 3),
+            (0, 2),
+            (0, 0),
+            (0, 0),
+        ]
+
+        for a, b in expectations:
+            gilded_rose.update_quality()
+            self.assertEqual(a, items[0].quality)
+            self.assertEqual(b, items[1].quality)
+
+    def test_aged_brie_gains_quality_and_eventually_maxes(self):
+        items = [Item("Aged Brie", 2, 0)]
+        gilded_rose = GildedRose(items)
+
+        for expected in [1, 2, 4, 6]:
+            gilded_rose.update_quality()
+            self.assertEqual(expected, items[0].quality)
+
+        while items[0].quality < 50:
+            gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+
+    def test_legendary_sulfuras_has_unwavering_quality(self):
+        items = [
+            Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            Item("Sulfuras, Hand of Ragnaros", 0, 80),
+        ]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        self.assertEqual(20, items[0].quality)
+        self.assertEqual(80, items[1].quality)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
